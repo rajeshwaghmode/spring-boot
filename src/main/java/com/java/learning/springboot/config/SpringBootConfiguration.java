5212423acf692c8b79686a551bd9b4a78a0f27c7
Rajesh <rajeshwaghmode3@gmail.com>
2020-07-19 00:36:22 +0530

    Spring Boot Actuator : Initial commit

---
+++ src/main/java/com/java/learning/springboot/config/SpringBootConfiguration.java
@@ -0,0 +1,9 @@
+package com.java.learning.springboot.config;
+
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SpringBootConfiguration {
+
+}
