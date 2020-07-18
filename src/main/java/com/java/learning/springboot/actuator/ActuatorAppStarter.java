de34f3b4b8a8af02c138af0d44b5028a55243c7e
Rajesh <rajeshwaghmode3@gmail.com>
2020-07-19 00:48:10 +0530

    Spring Boot Actuator : Initial commit

--- src/main/java/com/java/learning/springboot/actuator/ActuatorAppStarter.java
+++ src/main/java/com/java/learning/springboot/actuator/ActuatorAppStarter.java
@@ -1,11 +1,11 @@
-package com.java.learning.springboot;
+package com.java.learning.springboot.actuator;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ApplicationStarter {
+public class ActuatorAppStarter {
     public static void main(String ... args){
-        SpringApplication.run(ApplicationStarter.class);
+        SpringApplication.run(ActuatorAppStarter.class);
     }
 }
